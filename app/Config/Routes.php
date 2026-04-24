c5a1687f46386a6b9899e6b7566f88a25bdb0f50
Catur Wahyu M <caturwahyuum@gmail.com>
2026-04-24 21:52:05 +0700

    add session login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,4 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('/produk', 'ProdukController::index');
 $routes->get('/keranjang', 'TransaksiController::index');
+$routes->get('/login', 'Auth::login');
